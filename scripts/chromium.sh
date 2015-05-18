fe5746f85889343604e4cacadde57916b187a63d
JSidrach <juan.sidrach@gmail.com>
2015-05-18 12:55:08 +0200

    Change permissions (+x)

--- scripts/chromium.sh
+++ scripts/chromium.sh
@@ -5,7 +5,9 @@ sudo apt-get install chromium-browser -y
 
 # Set as default browser
 sudo sed -i 's/firefox.desktop/chromium-browser.desktop/g' /etc/gnome/defaults.list
-sudo sed -i 's/thunderbird.desktop/chromium-browser.desktop/g' /etc/gnome/defaults.list
+
+# Set GMail as the default mail provider
+sudo apt-get install gnome-gmail -y
 
 # Manually
 echo "import ublock list (files/ublock.txt) via chrome://extensions > uBlock origin"
