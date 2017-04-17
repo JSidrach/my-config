96893c200cb4e7f3abf0378155f8033fbf4422ba
J. Sidrach <juan.sidrach@gmail.com>
2017-04-17 11:19:19 -0700

    Remove some default packages, add some

--- MANUAL.md
+++ MANUAL.md
@@ -1,14 +1,13 @@
 List of things to do manually after installing:
 
-* Set pinned apps to: Data, Spotify, Terminal, Atom, Firefox.
+* Set pinned apps to: Folders, Spotify, Terminal, Atom, Firefox Developer Edition.
 * Set launchers to folders, set pinned folders.
 * Disable sound effects (kmix > Event Stream).
-* Log in into Firefox/Firefox Aurora, configure uBlock origin, configure appearance.
+* Log in into Firefox Developer Edition, configure uBlock origin, configure appearance.
 * Log in Spotify, make lists available offline, change display settings.
 * Set wallpaper.
-* Set fish as default terminal (> Terminal > Preferences).
+* Set zsh as default terminal (> Terminal > Preferences).
 * Boot directly: add ```GRUB_DISABLE_OS_PROBER=true``` to ```/etc/default/grub```.
-* Configure fish theme: ```fish_config```.
 * Set default apps.
 * Shortcut for open a new terminal, and show desktop.
 * Disable window thumbnails (> Compositor > Keep window thumbails > Never).
--- freshInstall.sh
+++ freshInstall.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
+# Uninstall default packages
+sudo pacman -Rs                                   \
+  firefox-kde                                     \
+  flashplugin                                     \
+  konversation                                    \
+  plymouth                                        \
+  plymouth-theme-manjaro-elegant                  \
+  thunderbird-kde                                 \
+  akonadi                                         \
+  akonadi-contacts                                \
+  digikam
+
 # Install packages from Manjaro
-sudo pacman -Sy       \
-  atom                \
-  dropbox             \
-  spotify             \
-  chromium            \
-  go                  \
-  rust                \
-  kmix                \
-  ruby-sass
+sudo pacman -Sy                                   \
+  atom                                            \
+  dropbox                                         \
+  spotify                                         \
+  chromium                                        \
+  go                                              \
+  rust                                            \
+  kmix                                            \
+  ruby-sass                                       \
+  manjaro-zsh-config
 
 # Install packages from AUR (requires configuration)
-yaourt -S          \
-  firefox-aurora   \
-  spotify          \
-  android-studio   \
-  google-cloud-sdk \
-  ruby-jekyll      \
-  leagueoflegends
+yaourt -S                                         \
+  firefox-developer                               \
+  spotify                                         \
+  android-studio                                  \
+  google-cloud-sdk                                \
+  ruby-jekyll                                     \
+  leagueoflegends-git
